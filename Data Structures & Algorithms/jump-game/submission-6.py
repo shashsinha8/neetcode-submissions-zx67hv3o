5bf2390fe07a74abecdd57b9852c03881687833f
Shashwat Sinha <136245743+shashsinha8@users.noreply.github.com>
2026-04-25 12:53:59 -0500

    Add: jump-game - submission-6

---
+++ Data Structures & Algorithms/jump-game/submission-6.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if not nums: 
+            return True
+        curr = nums[0]
+
+        if curr == nums[-1] and len(nums) == 1:
+            return True
+        elif curr >= len(nums) - 1:
+            return True
+        elif curr == 0 and len(nums) >= 1:
+            return False
+        
+
+        path = False
+        while curr:
+            if len(nums) > 1:
+                temp = self.canJump(nums[curr:])
+                path = path or temp
+                if path: 
+                    break
+                curr -= 1
+
+        return path
+
+
+"""
+nums = [1,2,0,1,0]
+curr = 1
+
+
+
+"""
+        
