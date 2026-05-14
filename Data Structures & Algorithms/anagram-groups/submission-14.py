1987437b53e2bdde272fc85a232324162d70e18f
Shashwat Sinha <136245743+shashsinha8@users.noreply.github.com>
2026-05-14 14:19:56 -0500

    Add: anagram-groups - submission-14

---
+++ Data Structures & Algorithms/anagram-groups/submission-14.py
@@ -0,0 +1,20 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        hashmap = defaultdict(list)
+
+        for s in strs: 
+            hashmap[self.counter(s)].append(s)
+
+        ans = [v for k, v in hashmap.items()]
+        
+        return ans
+    
+    def counter(self, s): 
+        ans = [0] * 26
+
+        for c in s: 
+            ans[ord(c) - ord("a")] += 1
+            
+        return tuple(ans)
