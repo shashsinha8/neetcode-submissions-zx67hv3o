ec47988672d60880bd7f7983e263ee8b49510b29
Shashwat Sinha <136245743+shashsinha8@users.noreply.github.com>
2026-05-14 17:38:24 -0500

    Add: top-k-elements-in-list - submission-17

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-17.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        count_map = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        
+        for n in nums: 
+            count_map[n] = 1 + count_map.get(n, 0)
+        
+        for num, count in count_map.items():
+            freq[count].append(num)
+
+        # print(f"frequency_map = {freq}")
+        ans = []
+        for index in range(len(freq) - 1, -1, -1):
+            for num in freq[index]:
+                ans.append(num)
+                if len(ans) == k: 
+                    return ans
